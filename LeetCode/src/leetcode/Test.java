03fdd1c47d858f7f98972f9e14a911d43dafc521
Jony <1034463801@qq.com>
2020-02-25 02:34:29 +0800

    test

--- LeetCode/src/leetcode/Test.java
+++ LeetCode/src/leetcode/Test.java
@@ -28,35 +28,7 @@ public class Test {
 //        String sss = sb.toString();
 //        System.out.println(sss);
 
-//        Console con = System.console();
-//        String name = con.readLine("user name:");
-//        char[] pass = con.readPassword("pass");
-//        Scanner in = new Scanner(Paths.get("/Users/chenxu/Desktop/ab.txt"),"UTF-8");
-//        System.out.println(in);
-//        PrintWriter out = new PrintWriter("/Users/chenxu/Desktop/abc.txt","UTF-8");
-//        System.out.println(out);
-//
-//        for(int i = 10;i>0;i--){
-//            System.out.println("last " + i +" s");
-//        }
 
-//        Integer i =3;
-//        switch (i) {
-//            case 1:
-//                System.out.println(1);
-//                break;
-//            case 2:
-//                System.out.println(2);
-//                break;
-//            default:
-//                System.out.println(444);
-//            case 3:
-//                System.out.println(3);
-//                return;
-//
-//        }
-//
-//        System.out.println("767");
 
         int a = 100;
         System.out.println(a);
